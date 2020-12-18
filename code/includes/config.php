5e1963934ea3848e5b3ec2ad03efcdec333570b3
tamiltalsan <46154844+tamiltalsan@users.noreply.github.com>
2020-12-18 16:26:00 +0530

    Update config.php

--- code/includes/config.php
+++ code/includes/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER','localhost');
+define('DB_SERVER','mysql');
 define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_PASS' ,'password');
 define('DB_NAME','newsportal');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
