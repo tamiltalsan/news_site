b5b237ae62ed415f170bfc30960e825753d611c1
EC2 Default User <ec2-user@ip-172-31-39-28.us-east-2.compute.internal>
2020-12-18 09:50:02 +0000

    new commit

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM php:7.2-fpm
+
+
+RUN apt-get update
+RUN docker-php-ext-install mysqli pdo pdo_mysql
+RUN docker-php-ext-enable mysqli
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#RUN docker-php-ext-install mysqli 
+#&& docker-php-ext-enable mysqli
+#RUN docker-php-ext-install pdo_mysql
+
---
+++ fun/includes/config.php
@@ -0,0 +1,17 @@
+<?php
+define('DB_SERVER','mysql');
+define('DB_USER','root');
+define('DB_PASS' ,'password');
+define('DB_NAME','newsportal');
+$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+// Check connection
+if (mysqli_connect_errno())
+{
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
+
+
+
+
+
